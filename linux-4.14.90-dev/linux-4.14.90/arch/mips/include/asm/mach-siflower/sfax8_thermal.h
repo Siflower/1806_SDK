6a86e1d8cbabafe4cf9d2425d2f5f6662ad0dfed
irving <irving.luo@siflower.com.cn>
2021-03-16 23:34:33 +0800

    RM#8873,1,sync master 1.1.6 update to github sdk 	2,add rep board,wifi fullmac add led 	3,update wifi/eth ko to fix some bug

--- linux-4.14.90-dev/linux-4.14.90/arch/mips/include/asm/mach-siflower/sfax8_thermal.h
+++ linux-4.14.90-dev/linux-4.14.90/arch/mips/include/asm/mach-siflower/sfax8_thermal.h
@@ -1,6 +1,11 @@
 #ifndef _SFAX8_THERMAL_H_
 #define _SFAX8_THERMAL_H_
 
+struct sfax8_zone_device_ops{
+	int (*get_temp)(void *, int *);
+	int (*get_trend)(void *,enum thermal_trend *);
+};
+
 struct sfax8_cooling_device_ops {
 	int (*get_max_state)(void *, unsigned long *);
 	int (*get_cur_state)(void *, unsigned long *);
@@ -9,8 +14,7 @@ struct sfax8_cooling_device_ops {
 
 #ifdef CONFIG_SFAX8_THERMAL
 int sfax8_thermal_sensor_register(struct device *dev,
-			  int (*get_temp)(void *, long *),
-			  int (*get_trend)(void *, long *), void *data);
+			struct sfax8_zone_device_ops *ops, void *data);
 void sfax8_thermal_sensor_unregister(struct device *dev);
 
 int sfax8_cooling_device_register(struct device *dev, void *devdata,
---
+++ openwrt-18.06/package/kernel/sf_smac/config/siwifi_umh_nothing.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#siwifi_umh.sh is for debugging when an assert occurred
+#But now it do nothing,just to reduce log such as "Failed to call /bin/siwifi_umh.sh (/bin/siwifi_umh.sh returned -2)"
+#The siwifi_umh.sh with actual content is added by siwifitools
